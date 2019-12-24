1db8f06cff46caf3868b943dd9f039e1d41d5708
tangchen0224 <58196940+tangchen0224@users.noreply.github.com>
2019-12-24 12:11:46 +0800

    IDictionary
    IDictionary

---
+++ IDictionnary/IDictionnary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using CDS006.IDictionaryDemo;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            // 创建字典并添加元素
+            IDictionary d = new SimpleDictionary(2);
+                d.Add("小小", 201801);
+                d.Add("大大", 201802);
+            Console.WriteLine("字典元素是否包含 '小小'? {0}", d.Contains("小小"));
+            
+            // 显示所有的 key/value 对
+            foreach (DictionaryEntry de in d)
+            {
+                Console.WriteLine("{0} {1} 。", de.Key, de.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ IDictionnary/IDictionnary/Properties/IDictionary.cs
@@ -0,0 +1,85 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDictionary
+{
+    /// <summary>
+    /// 这个类使用一个 DictionaryEntry 实体实现 “键/值对” 对象集合
+    /// </summary>
+    public class Dictionary : System.Collections.IDictionary
+    {
+        // 元素集合
+        private DictionaryEntry[] items;  // 元素集合
+        private Int32 ItemsInUse = 0;     // 元素使用的数量
+
+        /// <summary>
+        /// 构造函数：定义集合元素的数量
+        /// 在 SimpleDictionary 的生命周期里，这个数量是不能更改的。
+        /// </summary>
+        /// <param name="numItems">元素数量</param>
+        public Dictionary(Int32 numItems)
+        {
+            items = new DictionaryEntry[numItems];
+        }
+
+        public object this[object key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        #region IDictionary 成员，注意这些成员都是 IDictionary 已经定义的，在这里需要具体实现
+        public bool IsReadOnly { get { return false; } }
+
+        public bool IsFixedSize => throw new NotImplementedException();
+
+        public ICollection Keys => throw new NotImplementedException();
+
+        public ICollection Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void Add(object key, object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDictionaryEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
+    }
+}
